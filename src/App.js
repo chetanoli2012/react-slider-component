c37bcb7732ceb952fc6088f4ad76a905bd73383a
Chetan Oli <chetanoli@Chetans-MacBook-Pro.local>
2023-10-07 15:19:30 +0545

    React slider component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const images = [
+    {
+      image_url:
+        "https://img.freepik.com/free-photo/young-female-jacket-shorts-presenting-comparing-something-looking-confident-front-view_176474-37521.jpg?w=1800&t=st=1693037944~exp=1693038544~hmac=97e967909706f9b73b4b47d521acf54806f4b9b3efab6196bc8a69f07efff554",
+      caption: "Image 1",
+    },
+    {
+      image_url:
+        "https://img.freepik.com/free-photo/girl-grey-shirt-showing-something-her-hand_144627-51099.jpg?t=st=1693037931~exp=1693038531~hmac=63713e5a5cf2d23f53ca82b9996ad224ac6e92d0275a53b6debbe6523d7df020",
+      caption: "Image 2",
+    },
+    {
+      image_url:
+        "https://img.freepik.com/free-photo/young-lady-shirt-jacket-making-scales-gesture-looking-cheerful-front-view_176474-85195.jpg?t=st=1693037931~exp=1693038531~hmac=2f83b6689538e4056912c96f448163e9ef10998f48f671b7e50279f81611fbe6",
+      caption: "Image 3",
+    },
+    {
+      image_url:
+        "https://img.freepik.com/free-photo/girl-wide-opening-hands-giving-explanation-high-quality-photo_144627-60466.jpg?w=1800&t=st=1693038021~exp=1693038621~hmac=d4520cd86b2aea3e5dda765ede05bb53d70e18a574756d0f41a6806fe325d26d",
+      caption: "Image 4",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <SlideShow images={images} />
     </div>
   );
 }
+
+const SlideShow = ({ images }) => {
+  const [active, setActive] = useState(0);
+
+  const handleBulettedNavigation = (imageIndex) => {
+    setActive(imageIndex);
+  };
+
+  const hanldeNext = () => {
+    if (active < images.length - 1) {
+      setActive((prevVal) => prevVal + 1);
+    } else {
+      setActive(images.length - 1);
+    }
+  };
+
+  const hanldePrev = () => {
+    if (active > 0) {
+      setActive((prevVal) => prevVal - 1);
+    } else {
+      setActive(0);
+    }
+  };
+
+  return (
+    <div className="slideshow">
+      {images.map(function (image, imageIndex) {
+        return (
+          <Slide
+            {...image}
+            key={image.caption}
+            active={active === imageIndex}
+          />
+        );
+      })}
+      <div className="buletted-navigation">
+        {images.map(function (image, imageIndex) {
+          return (
+            <div
+              className={`dot ${active === imageIndex ? "active" : ""}`}
+              key={image.caption}
+              onClick={() => handleBulettedNavigation(imageIndex)}
+            />
+          );
+        })}
+      </div>
+      <div className="next-prev-navigation">
+        <div className="navigation next" onClick={hanldeNext}>
+          &gt;
+        </div>
+        <div className="navigation prev" onClick={hanldePrev}>
+          &lt;
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Slide = ({ image_url, caption, active }) => {
+  return (
+    <div className={`slide ${active ? "active" : ""}`}>
+      <img src={image_url} alt={caption}></img>
+      <span>{caption}</span>
+    </div>
+  );
+};
 
 export default App;
